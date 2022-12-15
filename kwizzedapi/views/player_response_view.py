6f6c053fb3f1cc584c7c74faad2fb023c2fc3f56
Brandon Herbison <herbisonbrandon@gmail.com>
2022-12-15 15:49:13 -0500

    optimized player responses

--- kwizzedapi/views/player_response_view.py
+++ kwizzedapi/views/player_response_view.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from kwizzedapi.models import PlayerResponse, Player, Answer
+from kwizzedapi.models import PlayerResponse, Player, Answer, Question
 
 
 class PlayerResponseView(ViewSet):
@@ -15,8 +15,16 @@ class PlayerResponseView(ViewSet):
         return Response(serializer.data, status=status.HTTP_200_OK)
     
     def list(self, request):
+        
 
         playerResponse = PlayerResponse.objects.all()
+        
+        most_recent = request.query_params.get("recent", None)
+        if most_recent is not None:
+            playerResponse = PlayerResponse.objects.filter(player=most_recent).order_by('-id')[:1]
+            serializer = PlayerResponseSerializer(playerResponse, many=True)
+            return Response(serializer.data , status=status.HTTP_200_OK)
+        
         serializer = PlayerResponseSerializer(playerResponse, many=True)
         return Response(serializer.data , status=status.HTTP_200_OK)
     
@@ -36,6 +44,27 @@ class PlayerResponseView(ViewSet):
         serializer = PlayerResponseSerializer(new_player_response, context={'request': request})
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+class AnswerQuestionSerializer(serializers.ModelSerializer):
+    """JSON serializer for answers
+
+    Arguments:
+        serializers
+    """
+    class Meta:
+        model = Question
+        fields = ('question_text',)
+    
+class AnswerPlayerResponseSerializer(serializers.ModelSerializer):
+    """JSON serializer for answers
+
+    Arguments:
+        serializers
+    """
+    question = AnswerQuestionSerializer(many=False)
+    class Meta:
+        model = Answer
+        fields = ('id', 'answer_text', 'is_correct', 'question',)
 
 class PlayerResponseSerializer(serializers.ModelSerializer):
     """JSON serializer for playerResponses
@@ -43,6 +72,8 @@ class PlayerResponseSerializer(serializers.ModelSerializer):
     Arguments:
         serializers
     """
+    answer = AnswerPlayerResponseSerializer(many=False)
+    
     class Meta:
         model = PlayerResponse
         fields = ('id', 'player', 'answer',)
